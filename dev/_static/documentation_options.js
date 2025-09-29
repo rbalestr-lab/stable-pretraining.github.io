9f17b3d51f3a1b4ab3dce33ae589242a4b355df2
Circle CI <circle@stable-pretraining.github.io>
2025-09-29 17:51:52 +0000

    CircleCI update of dev docs (1505).

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev12+g4dd5cdd8c.d20250929',
+    VERSION: '0.1.3.dev13+gde3ef501c.d20250929',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
